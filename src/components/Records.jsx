e2098cbe403bb0514adb9e7a6f2171bd4a80b060
Kevin Abey Issac <kevinabey04@gmail.com>
2024-07-11 16:28:38 +0530

    style changes

--- src/components/Records.jsx
+++ src/components/Records.jsx
@@ -27,10 +27,10 @@ const columns = [
     { id: 3, name: 'Ambu', department: 'Mechanical', semester: 4 },
     { id: 4, name: 'Alvin', department: 'Civil', semester: 2 },
     { id: 5, name: 'Varun', department: 'Computer Science', semester: 8 },
-    { id: 6, name: 'Rohit', department: 'Red Priestess', semester: 2 },
-    { id: 7, name: 'Gagan', department: 'Advisory', semester: 6 },
-    { id: 8, name: 'Aadarsh', department: 'Diplomacy', semester: 4 },
-    { id: 9, name: 'Riya', department: 'Espionage', semester: 8 },
+    { id: 6, name: 'Rohit', department: 'Civil', semester: 2 },
+    { id: 7, name: 'Gagan', department: 'Mechanical', semester: 6 },
+    { id: 8, name: 'Aadarsh', department: 'Computer Science', semester: 4 },
+    { id: 9, name: 'Riya', department: 'Electrical', semester: 8 },
   ];
   
   
